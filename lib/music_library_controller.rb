6a99aa51af754de2c9ef4521a152acf6debe00d1
Carlton Dwayne <carltondgrandison@gmail.com>
2019-04-11 02:55:35 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -35,6 +35,6 @@ class MusicLibraryController
   def list_artists
     Artist.all.sort_by{|artist| artist.name}.each.with_index(1) do |artist, i|
       puts "#{i}. #{artist.name}"
-    end 
+    end
   end
 end
